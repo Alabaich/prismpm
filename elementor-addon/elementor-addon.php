cced87f8ce00b14391fbc19d04bb49c2c96accf2
Alabaich <enjoyable.design@gmail.com>
2024-12-15 16:13:54 -0500

    CTA

--- elementor-addon/elementor-addon.php
+++ elementor-addon/elementor-addon.php
@@ -13,11 +13,12 @@ function register_hello_world_widget( $widgets_manager ) {
     require_once( __DIR__ . '/widgets/switchSideImage.php' );
     require_once( __DIR__ . '/widgets/totalServices.php' );
     require_once( __DIR__ . '/widgets/fullSizeImage.php' );
+    require_once( __DIR__ . '/widgets/applyCTAButton.php' );
 	
 
     $widgets_manager->register( new \Elementor_switchSideImage() );
     $widgets_manager->register( new \Elementor_totalServices() );
-    $widgets_manager->register( new \Elementor_fullSizeImage() );
+    $widgets_manager->register( new \Elementor_applyCTAButton() );
 
 
 }
