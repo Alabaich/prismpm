1337b9ff42d9a9d1abd91a744be9105ee9d1b3fe
ReddiJe <nikitosciba@gmail.com>
2024-12-23 10:30:11 +0100

    richtext

--- elementor-addon/elementor-addon.php
+++ elementor-addon/elementor-addon.php
@@ -15,6 +15,7 @@ function register_hello_world_widget( $widgets_manager ) {
     require_once( __DIR__ . '/widgets/fullSizeImage.php' );
     require_once( __DIR__ . '/widgets/applyCTAButton.php' );
     require_once( __DIR__ . '/widgets/propertyMapWidget.php' );
+    require_once( __DIR__ . '/widgets/richText.php' );
 	
 
     $widgets_manager->register( new \Elementor_switchSideImage() );
@@ -22,6 +23,7 @@ function register_hello_world_widget( $widgets_manager ) {
     $widgets_manager->register( new \Elementor_fullSizeImage() );
     $widgets_manager->register( new \Elementor_applyCTAButton() );
     $widgets_manager->register( new \Elementor_PropertyMapWidget() );
+    $widgets_manager->register( new \Elementor_richText() );
 
 
 }
